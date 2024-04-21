188d18069578a6653bc66efdde34e462e94bd4ff
ciara1b <ciara2barry@gmail.com>
2024-04-21 17:46:14 +0100

    Basic functionality implemented with no repeat abilities

--- FF16_Loadout_Randomizer.py
+++ FF16_Loadout_Randomizer.py
@@ -1,3 +1,5 @@
+from random import *
+
 feat_dict = {"Phoenix": "Phoenix Shift", 
              "Garuda": "Deadly Embrace",
              "Ramuh": "Blind Justice",
@@ -9,7 +11,7 @@ feat_dict = {"Phoenix": "Phoenix Shift",
              "Ultima": "Ascension"
              }
 
-ability_dict = {"Ifrit": ["Will o' the Wykes", "Ignition"],
+ability_dict = {"Ifrit": ["Will o' the Wykes", "Ignition", "", ""],
                 "Phoenix": ["Rising Flames", "Heatwave", "Flames of Rebirth", "Scarlet Cyclone"],
                 "Garuda": ["Gouge", "Wicked Wheel", "Rook's Gambit", "Aerial Blast"],
                 "Ramuh": ["Thunderstorm", "Judgement Bolt", "Lightning Rod", "Pile Drive"],
@@ -20,3 +22,30 @@ ability_dict = {"Ifrit": ["Will o' the Wykes", "Ignition"],
                 "Leviathan": ["Deluge", "Abyssal Tear", "Tsunami", "Cross Swell"],
                 "Ultima": ["Dominion", "Proselytize", "Voice of God", "Ultimate Demise"]
                 }
+
+def randomizer(feats, abilities):
+    loadout = {}
+    # choose 3 random feats
+    feat_set = sample(list(feats.values()), 3)
+
+    for feat in feat_set:
+        ability_one, abilities = random_ability_remove(abilities)
+        ability_two, abilities = random_ability_remove(abilities)
+
+        loadout[feat] = [ability_one, ability_two]
+
+    return loadout
+
+def random_ability_remove(abilities):
+    # choose a random eikon
+    ability_set = choice(list(abilities.items()))
+
+    # choose a random ability from that eikon
+    ability = choice(ability_set[1])
+
+    # remove that ability (list item) from it's respective eikon (key) in the dictionary
+    abilities[ability_set[0]] = ability_set[1].remove(ability)
+
+    return ability, abilities
+
+print(randomizer(feat_dict, ability_dict))
