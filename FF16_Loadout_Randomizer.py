1b010b5188959280cf4f54923321c35be9b09fb7
ciara1b <ciara2barry@gmail.com>
2024-04-21 16:30:53 +0100

    Init commit

---
+++ FF16_Loadout_Randomizer.py
@@ -0,0 +1,22 @@
+feat_dict = {"Phoenix": "Phoenix Shift", 
+             "Garuda": "Deadly Embrace",
+             "Ramuh": "Blind Justice",
+             "Titan": "Titanic Block",
+             "Bahamut": "Wings of Light",
+             "Shiva": "Cold Snap",
+             "Odin": "Arm of Darkness",
+             "Leviathan": "Serpent's Cry",
+             "Ultima": "Ascension"
+             }
+
+ability_dict = {"Ifrit": ["Will o' the Wykes", "Ignition"],
+                "Phoenix": ["Rising Flames", "Heatwave", "Flames of Rebirth", "Scarlet Cyclone"],
+                "Garuda": ["Gouge", "Wicked Wheel", "Rook's Gambit", "Aerial Blast"],
+                "Ramuh": ["Thunderstorm", "Judgement Bolt", "Lightning Rod", "Pile Drive"],
+                "Titan": ["Upheavel", "Raging Fists", "Windup", "Earthen Fury"],
+                "Bahamut": ["Flare Breath", "Impulse", "Satellite", "Gigaflare"],
+                "Shiva": ["Diamond Dust", "Rime", "Ice Age", "Mesmerize"],
+                "Odin": ["Gungnir", "Heaven's Cloud", "Rift Slip", "Dancing Steel"],
+                "Leviathan": ["Deluge", "Abyssal Tear", "Tsunami", "Cross Swell"],
+                "Ultima": ["Dominion", "Proselytize", "Voice of God", "Ultimate Demise"]
+                }
